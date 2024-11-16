2df8693fd4b9bdff69c9203978ea36cb0671f8eb
Derek Surguy <Surguy.Derek@gmail.com>
2024-11-16 12:53:23 -0600

    Add github webhook API route

--- app/api/hooks/github/route.ts
+++ app/api/hooks/github/route.ts
@@ -1,5 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
+import { Webhooks } from "@octokit/webhooks";
+
+const webhooks = new Webhooks({
+  secret: process.env.GITHUB_WEBHOOK_SECRET!,
+});
 
 export async function POST(request: NextRequest) {
-  return NextResponse.json({})
+  const body = await request.text();
+  const headers = request.headers;
+  const signature = headers.get("x-hub-signature-256");
+
+  if (!signature || !(await webhooks.verify(body, signature))) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  try {
+    console.log(JSON.parse(body));
+  } catch (e) {
+    console.error(e);
+  }
+
+  return new NextResponse(null, { status: 202 });
 }
