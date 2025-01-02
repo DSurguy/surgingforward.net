0f41685105208446e0c68357029ee9246cb25567
Derek Surguy <surguy.derek@startmail.com>
2025-01-02 17:25:31 -0600

    Change release header layout

--- app/releases/page.tsx
+++ app/releases/page.tsx
@@ -29,15 +29,15 @@ export default async function ReleasePage({ searchParams }: PageProps) {
       <div className="main-min-size p-4">{
         releases.map(release => (<article key={`${release.repositoryId}.${release.id}`} className="mb-8">
           <div className="border-b-2 border-slate-200 flex justify-between items-end">
+            <a className="font-mono mr-2 text-sm text-slate-700" href={release.repositoryHtmlUrl}>
+              <span>{release.repositoryHtmlUrl}</span>
+            </a>
             <span className="text-sm whitespace-nowrap bg-slate-200 p-1 rounded-sm font-mono">
               <span className="text-slate-500 mr-1">tag</span>
               <span className="">{release.tag}</span>
             </span>
-            <a className="font-mono mr-2 text-sm underline text-slate-700 text-end" href={release.repositoryHtmlUrl}>
-              <span>{release.repositoryHtmlUrl}</span>
-            </a>
           </div>
-          <h2 className="text-2xl font-semibold text-teal-800 py-2"><a href={release.html_url}>{release.name}</a></h2>
+          <h2 className="text-2xl font-semibold text-teal-800 py-2 underline"><a href={release.html_url}>{release.name}</a></h2>
           <div>
             <MDXRemote source={release.body} components={{
               p: props => <p className="py-2" {...props} />,
