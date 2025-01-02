3c71e22b648512e882df0888f6ec7396c40bea75
Derek Surguy <surguy.derek@startmail.com>
2025-01-02 11:24:14 -0600

    Try to reduce memory usage following docs

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,21 @@
 const nextConfig = {
   compress: false,
   transpilePackages: ['next-mdx-remote'],
-  webpack: (config) => {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
     config.externals.push('bun:sqlite');
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
     return config;
   },
+  experimental: {
+    webpackBuildWorker: true
+  }
 };
 
 export default nextConfig;
