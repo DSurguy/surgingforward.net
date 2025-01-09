414e5996b10bcd0c81cfb40859352eff42ff721d
Derek Surguy <surguy.derek@startmail.com>
2025-01-09 15:06:43 -0600

    Add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next'
+import LoadableContent from '@/sharedComponents/LoadableContent';
+
+export const metadata: Metadata = {
+  title: 'Derek Surguy | Contact',
+}
+
+export default function ContactPage() {
+  return <>
+    <LoadableContent>
+      <div>Test</div>
+    </LoadableContent>
+  </>
+}
