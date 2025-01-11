fd89ee06bbee2dfe66312982839a9cdfcfb146d8
Derek Surguy <surguy.derek@startmail.com>
2025-01-11 13:33:25 -0600

    Escape single quote because who knows why

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,7 +11,7 @@ export default function ProjectsPage() {
     <LoadableContent>
       <div className="p-4">
         <h1 className="text-3xl">Projects</h1>
-        <p className="mt-2">I've included a selection of my personal projects below, please explore and click into a project to learn more about it.</p>
+        <p className="mt-2">I&apos;ve included a selection of my personal projects below, please explore and click into a project to learn more about it.</p>
         <p className="mt-2 mb-4">Feel free to browse <a href="https://github.com/DSurguy" className="underline">my github</a> for more raw code and things that were too silly or bad to discuss here.</p>
         <ProjectList />
       </div>
