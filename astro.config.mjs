9d62776aa391f62c6726242958b7066dcf2fe5ea
Derek Surguy <surguy.derek@startmail.com>
2025-07-05 18:28:45 -0500

    Format and render resume to PDF

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,23 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel';
 
+import pdf from 'astro-pdf';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro.surgingforward.net',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), pdf({
+    pages: {
+      '/resume': [{
+        path: 'resume.pdf',
+        pdf: {
+          margin: {
+            top: 20
+          }
+        }
+      }]
+    }
+  })],
   outDir: './dist',
   cacheDir: './cache',
   prefetch: true,
